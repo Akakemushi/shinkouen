ddd2980eb34f6f568b44050503756235bd07d1ce
Jason Sturgeon <zergyjanus@gmail.com>
2024-04-05 20:07:42 +0900

    added teapot model

---
+++ app/models/teapot.rb
@@ -0,0 +1,2 @@
+class Teapot < ApplicationRecord
+end
---
+++ db/migrate/20240405094428_create_teapots.rb
@@ -0,0 +1,8 @@
+class CreateTeapots < ActiveRecord::Migration[7.1]
+  def change
+    create_table :teapots do |t|
+      t.
+      t.timestamps
+    end
+  end
+end
