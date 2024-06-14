b8f0e64939ba18f56eddcd1bd1a28f57cbc98715
Jason Sturgeon <zergyjanus@gmail.com>
2024-06-14 21:13:31 +0900

    implemented gem money-rails into project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_26_105640) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_14_102745) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,12 +49,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_26_105640) do
     t.integer "weight"
     t.integer "ccs"
     t.text "comment"
-    t.integer "price", null: false
+    t.integer "price_cents", default: 0, null: false
     t.string "kilntype", null: false
     t.string "shape", null: false
     t.string "maker", null: false
     t.integer "views", default: 0, null: false
     t.boolean "in_stock", default: true, null: false
+    t.string "sku", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
