f5a42a84631d116d8a70f91ff7119fb34a90546a
Jason Sturgeon <zergyjanus@gmail.com>
2024-08-13 18:37:00 +0900

    made actual cart items appear in the stripe form

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,26 +1,23 @@
 class CheckoutsController < ApplicationController
   def show
-    @session = Stripe::Checkout::Session.create(
-      line_items: [{
+    cart = current_user.cart
+    line_item_array = []
+    teapots_in_cart = cart.teapots.includes(:cart_item)
+    teapots_in_cart.each do |teapot|
+      next_line_item_hash = {
         price_data: {
           currency: "jpy",
           product_data: {
-            name: "yokode_teapot",
+            name: "#{teapot.shape} teapot by #{teapot.maker}"
           },
-          unit_amount: 12345
+          unit_amount: teapot.price_cents
         },
         quantity: 1
-      },
-      {
-        price_data: {
-          currency: "jpy",
-          product_data: {
-            name: "underwear",
-          },
-          unit_amount: 1111
-        },
-        quantity: 3
-      }],
+      }
+      line_item_array << next_line_item_hash
+    end
+    @session = Stripe::Checkout::Session.create(
+      line_items: line_item_array,
       mode: "payment",
       ui_mode: "embedded",
       return_url: CGI.unescape(payment_url(session_id: '{CHECKOUT_SESSION_ID}'))
