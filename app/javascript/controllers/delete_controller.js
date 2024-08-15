91b040a787b77d79fb301c8baf77c8b3dbb8e598
Jason Sturgeon <zergyjanus@gmail.com>
2024-08-15 18:22:17 +0900

    fixed the bug with the delete confirmation not working right.

--- app/javascript/controllers/delete_controller.js
+++ app/javascript/controllers/delete_controller.js
@@ -10,22 +10,31 @@ export default class extends Controller {
   }
 
   deleteTeapot() {
-    const teapotId = this.teapotIdValue
-    const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+    const teapotId = this.teapotIdValue;
+    const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
     fetch(`/teapots/${teapotId}`, {
       method: 'DELETE',
       headers: {
-        'X-CSRF-Token': token,
-        'Content-Type': 'application/json'
-      }
+        'X-CSRF-Token': token
+      },
+      redirect: 'follow'
     })
     .then(response => {
-      if (response.ok) {
-        window.location.href = '/teapots'
+      if (response.redirected) {
+        // If the response was redirected, navigate to the new URL.  This line wasn't present in previous versions and that caused problems
+        window.location.href = response.url;
+      } else if (response.ok) {
+        window.location.href = '/teapots';
       } else {
-        alert('There was an error deleting the teapot.')
+        return response.json().then(data => {
+          alert(data.error || 'There was an error deleting the teapot.');
+        });
       }
     })
+    .catch(error => {
+      console.error('Fetch error:', error);
+      alert('There was an error processing the request.');
+    });
   }
 }
